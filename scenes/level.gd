e37d963584de55b6cb5387c11c72ee64a49366ad
joebog1 <joebog96@gmail.com>
2024-01-06 16:22:38 +1030

    Adds collision between the ball and puck.

--- scenes/level.gd
+++ scenes/level.gd
@@ -2,7 +2,40 @@ extends Node2D
 
 const INITAL_BALL_SPEED = 400
 
+func _determine_new_ball_velocity(body):
+	# The new angle of the ball is not detemrined by the previous angular 
+	# velocity. It is entierly based off of the offeset between the ball and 
+	# the puck
+	#
+	#                         ^ <- New angle
+	#                          \  ______
+	#                           \ |    |
+	#  Centre of ball    ->      *|    |
+	#                             |\   |
+	#                             |  * | <- Centre of puck
+	#                             |    |
+	#                             |    |
+	#                             |    |
+	#                             ------
+	
+	var centre_of_puck = body.get_global_position()
+	
+	var centre_of_ball = $Ball.get_global_position()
+	print("puck: " + str(centre_of_puck) + 
+		  " \nball: " + str(centre_of_ball) + "\n")
+	# Determine the y difference and calulate a normalised vector from the 
+	# result.
+	var difference_vector = centre_of_ball - centre_of_puck
+	var result = difference_vector.normalized() * INITAL_BALL_SPEED
+	print("result: " + str(result) + "\n")
+	return result
+
 func _ready():
+	
+	# Santity check: Ensure that both pucks are the same size.
+	assert($PlayerPuck/CollisionShape2D.shape.get_rect().size == 
+		   $OpponentPuck/CollisionShape2D.shape.get_rect().size)
+	
 	# :NOTE: Perhaps this would be better if a signal to change the ball speed 
 	# was done instead of setting it directly here. Seems to work though 
 	# currently.
@@ -11,4 +44,4 @@ func _ready():
 func _on_ball_body_entered(body):
 	# :TODO: Do maths here to determine the new body velocity.
 	print(str($Ball.get_linear_velocity()))
-	$Ball.set_linear_velocity(-$Ball.get_linear_velocity())
+	$Ball.set_linear_velocity(_determine_new_ball_velocity(body))
