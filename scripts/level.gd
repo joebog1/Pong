6749216e318225da51d0afe8bfe2d0dcd45b5d04
joebog1 <joebog96@gmail.com>
2024-01-07 15:52:08 +1030

    Added "AI"
    Added documentation regarding the issue of the ball slowing down after hitting a roof or floor.

--- scripts/level.gd
+++ scripts/level.gd
@@ -2,6 +2,27 @@ extends Node2D
 
 const INITAL_BALL_SPEED = 400
 
+# Function takes the current ball position and OpponentPaddle and returns a 
+# normalised vector representing direction the OpponentPaddle should move in.
+# This is expected to be a vector with y movement only.
+# In the case the OpponentPaddle is perfectly lined up with the ball, (0,0) is 
+# returned.
+func determine_movement_direction():
+	# Take the current y positions of the Ball and OpponentPaddle
+	var resulting_direction: Vector2 =                                         \
+	Vector2(0,                                                                 \
+			$Ball.get_global_position().y >                                    \
+			$OpponentPaddle.get_global_position().y)
+	
+	assert(resulting_direction.x == 0)
+	# Since x is always 0 and the length must add up to 1 then y must be either 
+	# -1 or 1 or 0 in the case of the ball being perfectly lined up with the 
+	# paddle
+	assert(abs(resulting_direction.y) == 1 || resulting_direction.y == 0)
+	
+	return resulting_direction
+
+
 func _determine_new_ball_velocity(body, ball) -> Vector2:
 	
 	# :HACK: Using the editor description of nodes to do control flow is 
@@ -19,6 +40,11 @@ func _determine_new_ball_velocity(body, ball) -> Vector2:
 		#                      Angle A     *      Angle A
 		#      -----------------------------------------------------------
 		
+		# :BUG: move_and_slide() changes the linear velocity when a collision 
+		# occurs. What we really want is 
+		# ball.get_last_slide_collision().get_collider_velocity() but ball has 
+		# "lost" is knwolwedge of that as it was passed in as a Node2d, not as a
+		# CharacterBody2D. This means currently the behaviour is trash
 		var result = ball.get_linear_velocity()
 		result.y *= -1
 		return result
@@ -58,14 +84,23 @@ func _determine_new_ball_velocity(body, ball) -> Vector2:
 func _ready():
 	
 	# Santity check: Ensure that both Paddles are the same size.
-	assert($PlayerPuck/CollisionShape2D.shape.get_rect().size == 
-		   $OpponentPuck/CollisionShape2D.shape.get_rect().size)
+	assert($PlayerPaddle/CollisionShape2D.shape.get_rect().size == 
+		   $OpponentPaddle/CollisionShape2D.shape.get_rect().size)
 	
 	# :NOTE: Perhaps this would be better if a signal to change the ball speed 
 	# was done instead of setting it directly here. Seems to work though 
 	# currently.
 	$Ball.set_linear_velocity(Vector2(INITAL_BALL_SPEED, 0.0))
 
+func _process(delta):
+	# Determine the direction OpponentPaddle should move in.
+	var new_direction = determine_movement_direction()
+	
+	# :TODO: Should this be done with signals? hard to determine what is "right"
+	# in this context at the moment.
+	$OpponentPaddle.apply_movement(new_direction)
+	
 func _on_ball_body_entered(body):
 	print("Inital ball velocity: " + str($Ball.get_linear_velocity()) + "\n")
 	$Ball.set_linear_velocity(_determine_new_ball_velocity(body, $Ball))
+
--- scripts/opponent_puck.gd
+++ scripts/opponent_puck.gd
@@ -3,10 +3,17 @@ extends CharacterBody2D
 # TODO change based off of setting.
 const SPEED = 200.0
 
-# This function is expected to be called via signal from the level.
-# The Location argument is the location of the ball. This will 
-func apply_movement(Location):
+# :TODO: Write comments explaining behaviour
+# This function assumes that direction is normalised. An assert will fail if 
+# that's not the case.
+func apply_movement(direction):
+	
+	# If the direction is 0 we don't need to do anything and return early.
+	if(direction.length_squared() == 0):
+		return
+	
 	# TODO Do maths to figure out if we should go up or down. 
-	velocity = Vector2(0,Location.y);
+	assert(direction.length_squared() == 1)
+	velocity = direction * SPEED;
 	
 	move_and_slide()
